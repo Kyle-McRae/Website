ffca7dc6db5f87c96dedbbb51c492e9e984c1ffc
Kyle-McRae <kyle.mcrae1@gmail.com>
2023-11-01 11:17:03 -0700

    more about me info

--- react-amplified/src/components/Navbar/NavbarElements.js
+++ react-amplified/src/components/Navbar/NavbarElements.js
@@ -13,7 +13,7 @@ export const Nav = styled.nav`
 `;
  
 export const NavLink = styled(Link)`
-  color: #808080;
+  color: #ffffff;
   display: flex;
   align-items: center;
   text-decoration: none;
--- react-amplified/src/pages/home.js
+++ react-amplified/src/pages/home.js
@@ -4,18 +4,20 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="container">
-          
+          <br></br>
           <div className="imgCon">
             <img class="left" src="pumpkin.png" alt="pumpkin"></img>
             <img class="right" src="pumpkin.png" alt="pumpkin"></img>
           </div>
-          <h1>{'Kyle McRae'}</h1>
+          <h1>{'About Me'}</h1>
           <div className="imgCon">
             <img class="left" src="pumpkin.png" alt="pumpkin"></img>
             <img class="right" src="pumpkin.png" alt="pumpkin"></img>
           </div>
           <div className="main">
-            <p>I'm Kyle, and I am a seattle based software developer who graduated from Cal Poly. Welcome to my (seasonally decorated) website! It is entirely serverless, created in react, and hosted using AWS amplify.</p>
+            <p>I'm Kyle, and I am a Seattle based software developer who graduated from Cal Poly. Welcome to my (seasonally decorated) website! It is entirely serverless, created in React, and hosted using AWS Amplify.</p>
+            <p>In highschool, I was the lead programmer on my FRC team, 3070 Pronto, and I interned at my school's IT department. I then moved on to Cal Poly, where I got a computer science degree while also working part time as a software developer at Amazon.
+            </p>
           </div>
         </div>
       );
